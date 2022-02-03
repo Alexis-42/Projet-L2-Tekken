2f0ef1d1fda9ef92c512376322a832b585133d9c
Jck0day <s201110@ic2s130-04.univ-lemans.fr>
2022-02-03 11:27:53 +0100

    Indentation des fichiers Prise en compte de la taille de l'écran

--- src/deplacements.c
+++ src/deplacements.c
@@ -6,25 +6,25 @@
 #include "../include/deplacements.h"
 #include "../include/initSdl.h"
 
-void deplacementsInit(){
+void deplacementsInit() {
 
-SDL_Event event;
+  SDL_Event event;
 
-// SDL_WaitEvent(&event);
-SDL_PollEvent(&event);
-switch (event.type){
-case SDL_QUIT:
-quit = true;
-break;
-case SDL_KEYDOWN:
-switch(event.key.keysym.sym){
-case SDLK_q:
-x_destrec-=10;
-break;
-case SDLK_d:
-x_destrec+=10;
-break;
-}
+  // SDL_WaitEvent(&event);
+  SDL_PollEvent( & event);
+  switch (event.type) {
+  case SDL_QUIT:
+    quit = true;
+    break;
+  case SDL_KEYDOWN:
+    switch (event.key.keysym.sym) {
+    case SDLK_q:
+      x_destrec -= 10;
+      break;
+    case SDLK_d:
+      x_destrec += 10;
+      break;
+    }
 
-}
+  }
 }
