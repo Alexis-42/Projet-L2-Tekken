0d438e8b1ce8b8665e2f5339d0ef04dbea692866
Charly <jck0day@gmail.com>
2022-03-07 21:44:11 +0100

    Hitboxes Son (pas pu tester)

--- src/joueur.c
+++ src/joueur.c
@@ -7,13 +7,17 @@
 #include "../include/animations.h"
 
 void direction(Joueur * j1, Joueur * j2){
-    if(j1->direction==gauche)
+    if(j1->direction==gauche){ //Oui SDL c'est débile, ces deux if sont nécessaires sinon la hitbox est décalée physiquement pour le joueur regardant à gauche.
     	j1->hitbox.x+=j1->hitbox.w;
-    if(j2->direction==gauche)
+    }
+    if(j2->direction==gauche){
     	j2->hitbox.x+=j2->hitbox.w;
-
+    }
+    
     j1->direction=j1->hitbox.x<j2->hitbox.x; //Direction
     j2->direction=j1->hitbox.x>j2->hitbox.x;
+    
+    printf("PosX J1: %d, posX hitbox J1: %d, posX rect J1: %d\n", j1->position.x, j1->hitbox.x, j1->dstrect.x); //La hitbox est décalée alors que ses coords sont identiques à celles du joueur, le calcul plus haut la décale bêtement quand le joueur regarde à gauche mais ses coords ne sont plus les mêmes que celles du joueur.
 }
 
 void hitbox(Joueur * joueur){
