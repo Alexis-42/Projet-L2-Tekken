d7f4e57574a6b567df9e702ee96c93823bc5b914
Charly <jck0day@gmail.com>
2022-03-09 00:35:14 +0100

    Correction des déplacements cheloues là Ajout de la touche échap pour quitter

--- src/deplacements.c
+++ src/deplacements.c
@@ -10,7 +10,7 @@
 #include "../include/jeu.h"
 #include "../include/son.h"
 
-#define VITESSE 10
+#define VITESSE 1
 
 void sauter(Joueur * joueur){
 	int i, pos=joueur->position.y;
@@ -28,71 +28,70 @@ void attaquer(Joueur * jAttaquant, Joueur * j2){
 }
 
 void deplacements(Joueur * j1, Joueur * j2) {
-  direction(j1, j2);
-  SDL_Event event;
-  SDL_PollEvent(&event);
+	direction(j1, j2);
+	SDL_Event event;
+	SDL_PollEvent(&event);
 
-  switch (event.type) {
-  case SDL_QUIT:
-    quit = true;
-    break;
+	switch (event.type) {
+		case SDL_QUIT:
+		quit = true;
+		break;
 
-  case SDL_MOUSEBUTTONDOWN:
-    switch (event.button.button) {
-      case SDL_BUTTON_RIGHT:
-        j1->action=PARER;
-        break;
-      case SDL_BUTTON_LEFT:
-        attaquer(j1, j2);
-        break;
-  }
-  break;
+		case SDL_MOUSEBUTTONDOWN:
+		switch (event.button.button) {
+			case SDL_BUTTON_RIGHT:
+			j1->action=PARER;
+			break;
+			case SDL_BUTTON_LEFT:
+			attaquer(j1, j2);
+			break;
+		}
+		break;
 
-  case SDL_MOUSEBUTTONUP:
-  switch (event.button.button) {
-    case SDL_BUTTON_RIGHT:
-      j1->action=IDLE;
-      break;
-    case SDL_BUTTON_LEFT:
-      j1->action=IDLE;
-      break;
-    }
-    break;
+		case SDL_MOUSEBUTTONUP:
+		switch (event.button.button) {
+			case SDL_BUTTON_RIGHT:
+			j1->action=IDLE;
+			break;
+			case SDL_BUTTON_LEFT:
+			j1->action=IDLE;
+			break;
+		}
+		break;
 
-  case SDL_KEYDOWN:
-    switch (event.key.keysym.sym) {
-    case SDLK_q:
-      if(j1->position.x>0){
-        j1->position.x -= VITESSE;
-        j1->action=COURIR;
-       }
-      break;
-    case SDLK_d:
-      if(j1->position.x<ecran.w-j1->perso.taille_x){
-				//if(event.key.repeat == 1)
-        j1->position.x += VITESSE;
-        j1->action=COURIR;
+		/*
+case SDL_KEYUP:
+switch (event.key.keysym.sym) {
+case SDLK_q:
+j1->action=IDLE;
+break;
+case SDLK_d:
+j1->action=IDLE;
+break;
+case SDLK_z:
+j1->action=IDLE;
+break;
+}
+break;*/
+}
 
-       }
-      break;
-    case SDLK_SPACE:
-      sauter(j1);
-      break;
-    }
-	break;
-
-  case SDL_KEYUP:
-    switch (event.key.keysym.sym) {
-      case SDLK_q:
-      j1->action=IDLE;
-        break;
-      case SDLK_d:
-      j1->action=IDLE;
-        break;
-      case SDLK_z:
-      j1->action=IDLE;
-        break;
-    }
-    break;
+const Uint8 *state = SDL_GetKeyboardState(NULL);
+if (state[SDL_SCANCODE_A]) { //QWERTY C'EST TOTALEMENT CON
+	if(j1->position.x>0){
+		j1->position.x -= VITESSE;
+		j1->action=COURIR;
+	}
+}
+if (state[SDL_SCANCODE_D]) {
+	if(j1->position.x<ecran.w-j1->perso.taille_x){
+		j1->position.x += VITESSE;
+		j1->action=COURIR;
+	}
+}
+if (state[SDL_SCANCODE_SPACE]) {
+	sauter(j1);
+}
+if (state[SDL_SCANCODE_ESCAPE]) {
+	quit=true;
 }
 }
