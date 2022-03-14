913f104ee55881ed6db9a4805842785399eb27f3
Alexis Guimbert <alexisguimbert16@gmail.com>
2022-03-14 20:38:42 +0100

    On genere plus 3000 textures par secondes jeu plus fluide
    Former-commit-id: 91341134938e0748ec1f4601070252c91bc763ba

--- include/gui.h
+++ include/gui.h
@@ -2,7 +2,7 @@
 #ifndef GUI_H_INCLUDED__
 #define GUI_H_INCLUDED__
 
-void init_affichage_temps(int sec_deb_combat, TTF_Font* font, SDL_Rect * rect_sprite_pvj1, SDL_Texture **texture,SDL_Rect * rect_temps);
+void init_affichage_temps(int sec_deb_combat, int ancien_temps, TTF_Font* font, SDL_Rect * rect_sprite_pvj1, SDL_Texture **texture,SDL_Rect * rect_temps);
 void init_sprite_pv(SDL_Rect * rect_sprite_pv,int num_joueur);
 void init_afficher_nom_joueur(Joueur * joueur, TTF_Font* font, SDL_Rect * rect_sprite_pv, SDL_Rect * rect_nom_joueur, SDL_Texture ** texture);
 void barre_de_vie(Joueur * joueur, SDL_Rect * rect_sprite_pv, SDL_Texture * texture_barre_de_vie, SDL_Texture * texture_carre_rouge, SDL_Texture * texture_carre_jaune, int num_joueur, TTF_Font* font);
