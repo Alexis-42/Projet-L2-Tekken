e9bd84c31000b68911543664631631c2163e40a5
Guimbert Alexis <s200718@ic2s130-01.univ-lemans.fr>
2022-03-30 11:01:49 +0200

    documentation gui.c
    Former-commit-id: dfe32bafec6fd810db9209d711de36a940bb7723

--- src/gui.c
+++ src/gui.c
@@ -1,3 +1,11 @@
+/**
+ * @file gui.c
+ * @brief Fonctions relatives au gui ( Graphical User Interface )
+ * @date 2022-03-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <stdio.h>
 #include <string.h>
 #include <SDL2/SDL.h>
@@ -21,6 +29,15 @@ void init_gui(SDL_Texture ** texture_carre_jaune, SDL_Texture ** texture_carre_r
   *texture_barre_de_vie = SDL_CreateTextureFromSurface(renderer, sprite_barre_de_vie);
 }
 
+/**
+ * @brief Fonction qui initialise les textures qui sont en relation avec le temps
+ * 
+ * @param sec_deb_combat Le temps actuel du combat
+ * @param font La font que l'on veut
+ * @param rect_sprite_pvj1 Rectangle qui sera utilisé pour afficher la vie du joueur
+ * @param texture texture à entrer afin de la créer
+ * @param rect_temps Rectangle qui sera utilisé pour afficher le temps
+ */
 void init_affichage_temps(int sec_deb_combat,TTF_Font* font, SDL_Rect * rect_sprite_pvj1, SDL_Texture **texture,SDL_Rect * rect_temps){
  // sachant que ces deux temps sont en secondes on regenere les textures que si la seconde est differente
   char str_temps[3];
@@ -36,7 +53,12 @@ void init_affichage_temps(int sec_deb_combat,TTF_Font* font, SDL_Rect * rect_spr
   SDL_FreeSurface(surface);
 }
 
-// fonction qui permet l'init d'un sprite barre de vie en fonction du joueur
+/**
+ * @brief fonction qui permet l'init d'un sprite barre de vie en fonction du joueur
+ * 
+ * @param rect_sprite_pv Rectangle à initisialiser
+ * @param num_joueur Numero du joueur : @li 1 pour j1 @li 2 pour j2
+ */
 void init_sprite_pv(SDL_Rect * rect_sprite_pv, int num_joueur){
   rect_sprite_pv->y = 0;
   rect_sprite_pv->w = (int)(0.44*ecran.w);
@@ -48,6 +70,16 @@ void init_sprite_pv(SDL_Rect * rect_sprite_pv, int num_joueur){
   }
 }
 
+/**
+ * @brief Initialise les textures et rectangle qui sont en relation avec l'affichage du nom des joueurs
+ * 
+ * @param joueur Joueur auquel on va afficher le nom
+ * @param font Font à utiliser
+ * @param rect_sprite_pv Rectangle de la barre de vie afin d'avoir sa position
+ * @param rect_nom_joueur Rectangle dans lequel on va ecrire le nom du joueur ( en dessous de sa barre de vie )
+ * @param texture Texture à initialiser
+ * @param num_joueur Numero du joueur : @li 1 pour j1 @li 2 pour j2
+ */
 void init_afficher_nom_joueur(Joueur * joueur, TTF_Font* font, SDL_Rect * rect_sprite_pv, SDL_Rect * rect_nom_joueur, SDL_Texture ** texture, int num_joueur){
   int len_nom = strlen(joueur->perso.nom);
   SDL_Color textColor = {255, 255, 255, 0};
@@ -66,7 +98,16 @@ void init_afficher_nom_joueur(Joueur * joueur, TTF_Font* font, SDL_Rect * rect_s
   SDL_FreeSurface(surface);
 }
 
-// fonction à appeler pour afficher les barres de vies apres l'init ( à utiliser pour redessiner la barre à chaque appel )
+/**
+ * @brief Fonction à appeler pour afficher les barres de vies apres l'init ( à utiliser pour redessiner la barre à chaque appel )
+ * 
+ * @param joueur Joueur auquel on va afficher le nom
+ * @param rect_sprite_pv Rectangle de la partie grise de la barre de vie
+ * @param texture_barre_de_vie Texture de la partie grise de la barre de vie
+ * @param texture_carre_rouge Texture du carré rouge
+ * @param texture_carre_jaune Texture du carré jaune
+ * @param font Font à utiliser
+ */
 void barre_de_vie(Joueur * joueur, SDL_Rect * rect_sprite_pv, SDL_Texture * texture_barre_de_vie, SDL_Texture * texture_carre_rouge, SDL_Texture * texture_carre_jaune, TTF_Font* font){
     // SDL_rect de la barre de pv en fonction des pv
     SDL_Rect rect_pv,rect_pv_rouge,srcrect_pv_jaune;
