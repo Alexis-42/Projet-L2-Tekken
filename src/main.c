c05da3f209688d9b8497616ef6734374e1f373fc
Alexis Guimbert <alexisguimbert16@gmail.com>
2022-03-30 22:29:03 +0200

    fix
    Former-commit-id: ac227a63ae1b886a2444b32d1125488dbd90779c

--- src/main.c
+++ src/main.c
@@ -6,16 +6,11 @@
 #include "../include/menu_principal.h"
 #include "../include/jeu.h"
 
-bool debug = false;
 int main(int argc, char *argv[]) {
   // 1 si drip
   if(argc>=2){
     if(strcmp(argv[1],"drip")==0)
       menu_principal(1);
-    else {
-      debug = strcmp(argv[1], "debug")==0;
-      menu_principal(0);
-    }
   } else
     menu_principal(0);
   return 0;
